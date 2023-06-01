fb9e1f48cca28201d8e13134ca219511275fb7bb
adscam <43244566+tftftftft@users.noreply.github.com>
2023-06-01 11:35:20 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ pub fn obfuscate(_attr: TokenStream, item: TokenStream) -> TokenStream {
     TokenStream::from(quote! { #input })
 }
 
-// Your existing dead code generation code would go here, modified to return Block instead of syn::Block
+//dead code generation
 fn generate_dead_code() -> Block {
     let mut rng = rand::thread_rng();
 
